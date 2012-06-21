4283fe33b9593cdbf5a034b32993c570ee3c1d35
Jens Georg <jensg@openismus.com>
2012-06-21 23:49:48 +0200

    harmattan: Drop libffi dependency
    This seemed to cause problems in OVI store, so I replaced the generic
    marshaller by custom marshallers.

--- dbus/korva-dbus-interface.c
+++ dbus/korva-dbus-interface.c
@@ -17,6 +17,8 @@
 #include "gdbusobjectskeleton.h"
 #include "typedefs.h"
 
+#include "korva-marshal.h"
+
 /**
  * g_dbus_gvariant_to_gvalue:
  * @value: A #GVariant.
@@ -159,217 +161,6 @@ g_dbus_gvariant_to_gvalue (GVariant  *value,
       g_value_set_variant (out_gvalue, value);
       break;
     }
-}
-
-
-#undef G_OS_UNIX
-
-#include <ffi.h>
-static ffi_type *
-value_to_ffi_type (const GValue *gvalue, gpointer *value)
-{
-  ffi_type *rettype = NULL;
-  GType type = g_type_fundamental (G_VALUE_TYPE (gvalue));
-  g_assert (type != G_TYPE_INVALID);
-
-  switch (type)
-    {
-    case G_TYPE_BOOLEAN:
-    case G_TYPE_CHAR:
-    case G_TYPE_INT:
-    case G_TYPE_ENUM:
-      rettype = &ffi_type_sint;
-      *value = (gpointer)&(gvalue->data[0].v_int);
-      break;
-    case G_TYPE_UCHAR:
-    case G_TYPE_UINT:
-    case G_TYPE_FLAGS:
-      rettype = &ffi_type_uint;
-      *value = (gpointer)&(gvalue->data[0].v_uint);
-      break;
-    case G_TYPE_STRING:
-    case G_TYPE_OBJECT:
-    case G_TYPE_BOXED:
-    case G_TYPE_PARAM:
-    case G_TYPE_POINTER:
-    case G_TYPE_INTERFACE:
-    case G_TYPE_VARIANT:
-      rettype = &ffi_type_pointer;
-      *value = (gpointer)&(gvalue->data[0].v_pointer);
-      break;
-    case G_TYPE_FLOAT:
-      rettype = &ffi_type_float;
-      *value = (gpointer)&(gvalue->data[0].v_float);
-      break;
-    case G_TYPE_DOUBLE:
-      rettype = &ffi_type_double;
-      *value = (gpointer)&(gvalue->data[0].v_double);
-      break;
-    case G_TYPE_LONG:
-      rettype = &ffi_type_slong;
-      *value = (gpointer)&(gvalue->data[0].v_long);
-      break;
-    case G_TYPE_ULONG:
-      rettype = &ffi_type_ulong;
-      *value = (gpointer)&(gvalue->data[0].v_ulong);
-      break;
-    case G_TYPE_INT64:
-      rettype = &ffi_type_sint64;
-      *value = (gpointer)&(gvalue->data[0].v_int64);
-      break;
-    case G_TYPE_UINT64:
-      rettype = &ffi_type_uint64;
-      *value = (gpointer)&(gvalue->data[0].v_uint64);
-      break;
-    default:
-      rettype = &ffi_type_pointer;
-      *value = NULL;
-      g_warning ("value_to_ffi_type: Unsupported fundamental type: %s", g_type_name (type));
-      break;
-    }
-  return rettype;
-}
-
-static void
-value_from_ffi_type (GValue *gvalue, gpointer *value)
-{
-  switch (g_type_fundamental (G_VALUE_TYPE (gvalue)))
-    {
-    case G_TYPE_INT:
-      g_value_set_int (gvalue, *(gint*)value);
-      break;
-    case G_TYPE_FLOAT:
-      g_value_set_float (gvalue, *(gfloat*)value);
-      break;
-    case G_TYPE_DOUBLE:
-      g_value_set_double (gvalue, *(gdouble*)value);
-      break;
-    case G_TYPE_BOOLEAN:
-      g_value_set_boolean (gvalue, *(gboolean*)value);
-      break;
-    case G_TYPE_STRING:
-      g_value_set_string (gvalue, *(gchar**)value);
-      break;
-    case G_TYPE_CHAR:
-      g_value_set_char (gvalue, *(gchar*)value);
-      break;
-    case G_TYPE_UCHAR:
-      g_value_set_uchar (gvalue, *(guchar*)value);
-      break;
-    case G_TYPE_UINT:
-      g_value_set_uint (gvalue, *(guint*)value);
-      break;
-    case G_TYPE_POINTER:
-      g_value_set_pointer (gvalue, *(gpointer*)value);
-      break;
-    case G_TYPE_LONG:
-      g_value_set_long (gvalue, *(glong*)value);
-      break;
-    case G_TYPE_ULONG:
-      g_value_set_ulong (gvalue, *(gulong*)value);
-      break;
-    case G_TYPE_INT64:
-      g_value_set_int64 (gvalue, *(gint64*)value);
-      break;
-    case G_TYPE_UINT64:
-      g_value_set_uint64 (gvalue, *(guint64*)value);
-      break;
-    case G_TYPE_BOXED:
-      g_value_set_boxed (gvalue, *(gpointer*)value);
-      break;
-    case G_TYPE_ENUM:
-      g_value_set_enum (gvalue, *(gint*)value);
-      break;
-    case G_TYPE_FLAGS:
-      g_value_set_flags (gvalue, *(guint*)value);
-      break;
-    case G_TYPE_PARAM:
-      g_value_set_param (gvalue, *(gpointer*)value);
-      break;
-    case G_TYPE_OBJECT:
-      g_value_set_object (gvalue, *(gpointer*)value);
-      break;
-    default:
-      g_warning ("value_from_ffi_type: Unsupported fundamental type: %s",
-                g_type_name (g_type_fundamental (G_VALUE_TYPE (gvalue))));
-    }
-}
-
-/**
- * g_cclosure_marshal_generic:
- * @closure: A #GClosure.
- * @return_gvalue: A #GValue to store the return value. May be %NULL
- *   if the callback of closure doesn't return a value.
- * @n_param_values: The length of the @param_values array.
- * @param_values: An array of #GValue<!-- -->s holding the arguments
- *   on which to invoke the callback of closure.
- * @invocation_hint: The invocation hint given as the last argument to
- *   g_closure_invoke().
- * @marshal_data: Additional data specified when registering the
- *   marshaller, see g_closure_set_marshal() and
- *   g_closure_set_meta_marshal()
- *
- * A generic marshaller function implemented via <ulink
- * url="http://sourceware.org/libffi/">libffi</ulink>.
- *
- * Since: 2.30
- */
-void
-g_cclosure_marshal_generic (GClosure     *closure,
-                            GValue       *return_gvalue,
-                            guint         n_param_values,
-                            const GValue *param_values,
-                            gpointer      invocation_hint,
-                            gpointer      marshal_data)
-{
-  ffi_type *rtype;
-  void *rvalue;
-  int n_args;
-  ffi_type **atypes;
-  void **args;
-  int i;
-  ffi_cif cif;
-  GCClosure *cc = (GCClosure*) closure;
-
-  if (return_gvalue && G_VALUE_TYPE (return_gvalue))
-    {
-      rtype = value_to_ffi_type (return_gvalue, &rvalue);
-    }
-  else
-    {
-      rtype = &ffi_type_void;
-    }
-
-  rvalue = g_alloca (MAX (rtype->size, sizeof (ffi_arg)));
-
-  n_args = n_param_values + 1;
-  atypes = g_alloca (sizeof (ffi_type *) * n_args);
-  args =  g_alloca (sizeof (gpointer) * n_args);
-
-  if (G_CCLOSURE_SWAP_DATA (closure))
-    {
-      atypes[n_args-1] = value_to_ffi_type (param_values + 0,
-                                            &args[n_args-1]);
-      atypes[0] = &ffi_type_pointer;
-      args[0] = &closure->data;
-    }
-  else
-    {
-      atypes[0] = value_to_ffi_type (param_values + 0, &args[0]);
-      atypes[n_args-1] = &ffi_type_pointer;
-      args[n_args-1] = &closure->data;
-    }
-
-  for (i = 1; i < n_args - 1; i++)
-    atypes[i] = value_to_ffi_type (param_values + i, &args[i]);
-
-  if (ffi_prep_cif (&cif, FFI_DEFAULT_ABI, n_args, rtype, atypes) != FFI_OK)
-    return;
-
-  ffi_call (&cif, marshal_data ? marshal_data : cc->callback, rvalue, args);
-
-  if (return_gvalue && G_VALUE_TYPE (return_gvalue))
-    value_from_ffi_type (return_gvalue, rvalue);
 }
 
 
@@ -899,7 +690,7 @@ korva_controller1_default_init (KorvaController1Iface *iface)
     G_STRUCT_OFFSET (KorvaController1Iface, handle_get_devices),
     g_signal_accumulator_true_handled,
     NULL,
-    g_cclosure_marshal_generic,
+    korva_marshal_BOOLEAN__OBJECT,
     G_TYPE_BOOLEAN,
     1,
     G_TYPE_DBUS_METHOD_INVOCATION);
@@ -922,7 +713,7 @@ korva_controller1_default_init (KorvaController1Iface *iface)
     G_STRUCT_OFFSET (KorvaController1Iface, handle_get_device_info),
     g_signal_accumulator_true_handled,
     NULL,
-    g_cclosure_marshal_generic,
+    korva_marshal_BOOLEAN__OBJECT_STRING,
     G_TYPE_BOOLEAN,
     2,
     G_TYPE_DBUS_METHOD_INVOCATION, G_TYPE_STRING);
@@ -946,7 +737,7 @@ korva_controller1_default_init (KorvaController1Iface *iface)
     G_STRUCT_OFFSET (KorvaController1Iface, handle_push),
     g_signal_accumulator_true_handled,
     NULL,
-    g_cclosure_marshal_generic,
+    korva_marshal_BOOLEAN__OBJECT_VARIANT_STRING,
     G_TYPE_BOOLEAN,
     3,
     G_TYPE_DBUS_METHOD_INVOCATION, G_TYPE_VARIANT, G_TYPE_STRING);
@@ -969,7 +760,7 @@ korva_controller1_default_init (KorvaController1Iface *iface)
     G_STRUCT_OFFSET (KorvaController1Iface, handle_unshare),
     g_signal_accumulator_true_handled,
     NULL,
-    g_cclosure_marshal_generic,
+    korva_marshal_BOOLEAN__OBJECT_STRING,
     G_TYPE_BOOLEAN,
     2,
     G_TYPE_DBUS_METHOD_INVOCATION, G_TYPE_STRING);
@@ -990,7 +781,7 @@ korva_controller1_default_init (KorvaController1Iface *iface)
     G_STRUCT_OFFSET (KorvaController1Iface, device_available),
     NULL,
     NULL,
-    g_cclosure_marshal_generic,
+    g_cclosure_marshal_VOID__VARIANT,
     G_TYPE_NONE,
     1, G_TYPE_VARIANT);
 
@@ -1009,7 +800,7 @@ korva_controller1_default_init (KorvaController1Iface *iface)
     G_STRUCT_OFFSET (KorvaController1Iface, device_unavailable),
     NULL,
     NULL,
-    g_cclosure_marshal_generic,
+    g_cclosure_marshal_VOID__STRING,
     G_TYPE_NONE,
     1, G_TYPE_STRING);
 
